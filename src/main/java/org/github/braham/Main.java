38c905992c08077eab9a6844a71e539c82efc231
mohamed aziz braham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-10-16 09:15:59 +0200

    3eme commit

--- src/main/java/org/github/braham/Main.java
+++ src/main/java/org/github/braham/Main.java
@@ -4,7 +4,7 @@ package org.github.braham;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     //aziz
-    //deuxiéme modif
+    //deuxiéme
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
