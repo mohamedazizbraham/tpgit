1947cf5348df578701bd4abed2f7a624ebf823c6
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-16 09:32:57 +0200

    changement Alexis

--- src/main/java/org/github/braham/Main.java
+++ src/main/java/org/github/braham/Main.java
@@ -5,6 +5,7 @@ package org.github.braham;
 public class Main {
     //aziz
     //deuxiéme
+    //alexis
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
