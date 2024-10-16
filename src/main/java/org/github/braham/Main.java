a6cf8be609105c5591847275a71cb5da3de30b81
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-16 09:37:36 +0200

    test conflit

--- src/main/java/org/github/braham/Main.java
+++ src/main/java/org/github/braham/Main.java
@@ -6,6 +6,7 @@ public class Main {
     //aziz
     //deuxiéme
     //alexis
+    // TEST CONFLIT ALEXIS
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
